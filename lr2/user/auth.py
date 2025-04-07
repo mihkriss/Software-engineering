aaa92ccf3f7a3a7a5dfaaa72d180f13ff4249911
mihkriss <74809078+mihkriss@users.noreply.github.com>
2025-04-07 16:36:01 +0300

    Add files via upload

---
+++ lr2/user/auth.py
@@ -0,0 +1,72 @@
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from fastapi import  APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from typing import Optional
+# from user.user_service import user_db
+from passlib.context import CryptContext
+
+import os
+
+router = APIRouter()
+
+client_db = {
+    "admin":  "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW"
+    }
+
+SECRET_KEY = os.getenv("SECRET_JWT", "secret")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30 
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token") 
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+async def get_current_client(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        else:
+            return username
+    except JWTError:
+        raise credentials_exception
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+@router.post("/token")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    password_check = False
+    if form_data.username in client_db:
+        password = client_db[form_data.username]
+        if pwd_context.verify(form_data.password, password):
+            password_check = True
+
+    if password_check:
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(data={"sub": form_data.username}, expires_delta=access_token_expires)
+        return {
+        "message": "Authentication successful",
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
