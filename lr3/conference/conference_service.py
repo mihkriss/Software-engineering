16c24aee9b0cfa8412370d3e005fb24f642923a3
mihkriss <74809078+mihkriss@users.noreply.github.com>
2025-04-10 15:47:54 +0300

    Update conference_service.py

--- lr3/conference/conference_service.py
+++ lr3/conference/conference_service.py
@@ -1,17 +1,23 @@
-from fastapi import HTTPException, APIRouter, Depends
-from conference.conference_model import DocladCreate, DocladResponse, RoomCreate, RoomResponse
+from fastapi import HTTPException, FastAPI, Depends
+from .conference_model import DocladCreate, DocladResponse, RoomCreate, RoomResponse
 from typing import List
 from sqlalchemy.orm import Session
 from db.database import Doclads, Room
 from sqlalchemy.orm import Session
 from db.sessions import get_db
-from user.auth import get_current_client
+from auth.auth import get_current_client 
+
 from db.database import User
+from auth.auth import router as auth_touter
 
+app = FastAPI()
+app.include_router(auth_touter, prefix="/auth", tags=["auth"])
 
-router = APIRouter()
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome Conferences website"}
 
-@router.post("/doclads/", response_model=DocladResponse)
+@app.post("/doclads/", response_model=DocladResponse)
 async def create_doclad(doclad: DocladCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
     doclad =Doclads(title=doclad.title, author=doclad.author)
 
@@ -21,19 +27,19 @@ async def create_doclad(doclad: DocladCreate, db: Session = Depends(get_db), cur
     return doclad
 
 
-@router.get("/doclads_all/", response_model=List[DocladResponse])
+@app.get("/doclads_all/", response_model=List[DocladResponse])
 async def get_doclad_all(db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
     doclads = db.query(Doclads).order_by(Doclads.id).all()
     return doclads
 
-@router.get('/search/{doclad_title}', response_model=List[DocladResponse])
+@app.get('/search/{doclad_title}', response_model=List[DocladResponse])
 async def get_title(title: str, db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
     doclads = db.query(Doclads).order_by(Doclads.id).filter(Doclads.title.ilike(f"%{title}%")).all()
     if not doclads:
         raise HTTPException(status_code=404, detail="Doclad not found")
     return doclads
 
-@router.put("/{doclad_id}", response_model=DocladResponse)
+@app.put("/{doclad_id}", response_model=DocladResponse)
 async def update_doclad(doclad_id: int, update: DocladCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
     doclad = db.get(Doclads, doclad_id)
     if not doclad:
@@ -48,7 +54,7 @@ async def update_doclad(doclad_id: int, update: DocladCreate, db: Session = Depe
     db.refresh(doclad)
     return doclad
     
-@router.delete("/{doclad_id}", response_model=DocladResponse)
+@app.delete("/{doclad_id}", response_model=DocladResponse)
 async def delete_doclad(doclad_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
     doclad = db.get(DocladResponse, doclad_id)
     if not doclad:
@@ -57,7 +63,7 @@ async def delete_doclad(doclad_id: int, db: Session = Depends(get_db), current_u
     db.commit()
     return doclad
 
-@router.post("/rooms/", response_model=RoomResponse)
+@app.post("/rooms/", response_model=RoomResponse)
 async def create_room(rooms: RoomCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
     room = Room(**rooms.model_dump())
     db.add(room)
@@ -65,12 +71,12 @@ async def create_room(rooms: RoomCreate, db: Session = Depends(get_db), current_
     db.refresh(room)
     return room
 
-@router.get("/rooms_all/", response_model=List[RoomResponse])
+@app.get("/rooms_all/", response_model=List[RoomResponse])
 async def get_all_rooms(db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
     rooms = db.query(Room).order_by(Room.id).all()
     return rooms
 
-@router.put("/doclads/{doclad_id}/add_to_room/{room_id}", response_model=DocladResponse)
+@app.put("/doclads/{doclad_id}/add_to_room/{room_id}", response_model=DocladResponse)
 async def add_doclad_to_room(doclad_id: int, room_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
     doclad = db.get(Doclads, doclad_id)
     if not doclad:
@@ -85,7 +91,7 @@ async def add_doclad_to_room(doclad_id: int, room_id: int, db: Session = Depends
     db.refresh(doclad)
     return doclad
 
-@router.get("/{room_id}/doclads", response_model=List[DocladResponse])
+@app.get("/{room_id}/doclads", response_model=List[DocladResponse])
 async def get_conference_doclads(room_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
     room= db.get(Room, room_id)
     if not room: 
