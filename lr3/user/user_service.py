c1ba97c75647e4b1e6f35c3eb6925599fec922d8
mihkriss <74809078+mihkriss@users.noreply.github.com>
2025-04-10 15:50:30 +0300

    Update user_service.py

--- lr3/user/user_service.py
+++ lr3/user/user_service.py
@@ -1,81 +1,99 @@
-from fastapi import HTTPException, Query, APIRouter, Depends
-from .users_model import UserCreate, UserResponse, UserUpdate
-from sqlalchemy.orm import Session
-from db.database import User
+from fastapi import HTTPException, FastAPI, Depends
+from .conference_model import DocladCreate, DocladResponse, RoomCreate, RoomResponse
 from typing import List
+from sqlalchemy.orm import Session
+from db.database import Doclads, Room
+from sqlalchemy.orm import Session
 from db.sessions import get_db
-from sqlalchemy import select
-from passlib.context import CryptContext
-from .auth import get_current_client
+from auth.auth import get_current_client 
 
+from db.database import User
+from auth.auth import router as auth_touter
 
-router = APIRouter()
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+app = FastAPI()
+app.include_router(auth_touter, prefix="/auth", tags=["auth"])
 
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome User"}
 
-@router.post("/users/", response_model=UserResponse)
-async def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    if db.query(User).filter(User.login == user.login).first():
-        raise HTTPException(status_code=400, detail="Login already registered")
-    
-    hashed_password = pwd_context.hash(user.password)
+@app.post("/doclads/", response_model=DocladResponse)
+async def create_doclad(doclad: DocladCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
+    doclad =Doclads(title=doclad.title, author=doclad.author)
 
-    user = User(
-        username=user.username,
-        login=user.login,
-        password=hashed_password,
-        role=user.role
-    )
-   
-    db.add(user)
+    db.add(doclad)
     db.commit()
-    db.refresh(user)
-    return user
+    db.refresh(doclad)
+    return doclad
 
-@router.get("/{users_all}/", response_model=List[UserResponse])
-async def get_user_all(db: Session = Depends(get_db)):
-    users = db.query(User).order_by(User.id).all()
-    return users
 
-@router.get("/{user_login}", response_model=UserResponse)
-async def get_user_login(user_login: str, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.login == user_login).first()
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+@app.get("/doclads_all/", response_model=List[DocladResponse])
+async def get_doclad_all(db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
+    doclads = db.query(Doclads).order_by(Doclads.id).all()
+    return doclads
 
-@router.get('/search/{username}', response_model=List[UserResponse])
-async def get_user_name(username: str, db: Session = Depends(get_db)):
-    users = db.query(User).order_by(User.id).filter(User.username.ilike(f"%{username}%")).all()
-    if not users:
-        raise HTTPException(status_code=404, detail="User not found")
-    return users
+@app.get('/search/{doclad_title}', response_model=List[DocladResponse])
+async def get_title(title: str, db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
+    doclads = db.query(Doclads).order_by(Doclads.id).filter(Doclads.title.ilike(f"%{title}%")).all()
+    if not doclads:
+        raise HTTPException(status_code=404, detail="Doclad not found")
+    return doclads
 
-@router.put("/users/{user_id}", response_model=UserResponse)
-async def update_user(user_id: int, update: UserUpdate, db: Session = Depends(get_db),current_user: User = Depends(get_current_client)):
-    user = db.get(User, user_id)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+@app.put("/{doclad_id}", response_model=DocladResponse)
+async def update_doclad(doclad_id: int, update: DocladCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
+    doclad = db.get(Doclads, doclad_id)
+    if not doclad:
+        raise HTTPException(status_code=404, detail="Doclad not found")
 
     update_data = update.model_dump(exclude_unset=True)
-    hashed_password = pwd_context.hash(update_data["password"])
-
-    if "password" in update_data:
-        update_data["password"] = hashed_password
 
     for field, value in update_data.items():
-        setattr(user, field, value)
+        setattr(doclad, field, value)
 
     db.commit()
-    db.refresh(user)
-    return user
+    db.refresh(doclad)
+    return doclad
     
-
-@router.delete("/{user_id}", response_model=UserResponse, )
-async def delete_user(user_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
-    user = db.get(User, user_id)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    db.delete(user)
+@app.delete("/{doclad_id}", response_model=DocladResponse)
+async def delete_doclad(doclad_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
+    doclad = db.get(DocladResponse, doclad_id)
+    if not doclad:
+        raise HTTPException(status_code=404, detail="Doclad not found")
+    db.delete(doclad)
     db.commit()
-    return user
+    return doclad
+
+@app.post("/rooms/", response_model=RoomResponse)
+async def create_room(rooms: RoomCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
+    room = Room(**rooms.model_dump())
+    db.add(room)
+    db.commit()
+    db.refresh(room)
+    return room
+
+@app.get("/rooms_all/", response_model=List[RoomResponse])
+async def get_all_rooms(db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
+    rooms = db.query(Room).order_by(Room.id).all()
+    return rooms
+
+@app.put("/doclads/{doclad_id}/add_to_room/{room_id}", response_model=DocladResponse)
+async def add_doclad_to_room(doclad_id: int, room_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
+    doclad = db.get(Doclads, doclad_id)
+    if not doclad:
+        raise HTTPException(status_code=404, detail="Doclad not found")
+    
+    room = db.get(Room, room_id)
+    if not room:
+        raise HTTPException(status_code=404, detail="Room not found")
+    
+    doclad.room_id = room_id
+    db.commit()
+    db.refresh(doclad)
+    return doclad
+
+@app.get("/{room_id}/doclads", response_model=List[DocladResponse])
+async def get_conference_doclads(room_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_client)):
+    room= db.get(Room, room_id)
+    if not room: 
+        raise HTTPException(status_code=404, detail="Room not found")
+    return room.doclads
