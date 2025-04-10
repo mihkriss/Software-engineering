c1bde764b297057efbd0fea5e7bf884fae97fded
mihkriss <74809078+mihkriss@users.noreply.github.com>
2025-04-10 16:48:07 +0300

    Update database.py

--- lr3/db/database.py
+++ lr3/db/database.py
@@ -1,6 +1,6 @@
 
 from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy import String, ForeignKey, create_engine, Column, Integer
+from sqlalchemy import String, ForeignKey, create_engine, Column, Integer, Index
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/conference")
@@ -20,12 +20,21 @@ class User(Base):
     password = Column(String)
     role = Column(String)
 
+    __table_args__ = (
+        Index('ix_users_username', 'username', postgresql_using='gin', postgresql_ops={"username": "gin_trgm_ops"}),
+        Index('ix_users_login', 'login', postgresql_using='gin', postgresql_ops={"login": "gin_trgm_ops"}),
+    )
+
 class Room(Base):
     __tablename__ = "rooms"
     
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     doclads = relationship("Doclads", back_populates="room")
+
+    __table_args__ = (
+        Index('ix_rooms_title', 'title', postgresql_using='gin', postgresql_ops={"title": "gin_trgm_ops"}),
+    )
 
 class Doclads(Base):
     __tablename__ = "doclads"
@@ -36,4 +45,8 @@ class Doclads(Base):
     room_id = Column(Integer, ForeignKey("rooms.id"), nullable=True)
 
     room = relationship("Room", back_populates="doclads")
+
+    __table_args__ = (
+        Index('ix_doclads_title', 'title', postgresql_using='gin', postgresql_ops={"title": "gin_trgm_ops"}),
+    )
 
